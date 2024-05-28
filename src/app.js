5a5b5dd1f4b9b2261bb53b9aae69fc647633f38b
achint <achintj96244@gmail.com>
2024-05-28 22:48:50 +0530

    chore: Update CORS configuration to handle wildcard origin and credentials

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN === "*" ? true : process.env.CORS_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
