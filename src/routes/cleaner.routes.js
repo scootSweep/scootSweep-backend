7fb4ea4806399337cf747346041a07d26d29486c
achint <achintj96244@gmail.com>
2024-05-29 09:06:27 +0530

    feat: Add cleaner routes for property location retrieval

---
+++ src/routes/cleaner.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { verifyCleaner } from "../middlewares/auth.middleware.js";
+import cleanerService from "../services/cleaner.service.js";
+const router = Router();
+router.use(verifyCleaner);
+
+router.route("/property-location").get(cleanerService.getAllPropertyLocation);
+
+export default router;
