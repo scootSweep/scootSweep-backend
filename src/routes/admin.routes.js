5e7dea10f9a77fc32a9e58ea1913d2e1ab9dcbf0
achint <achintj96244@gmail.com>
2024-05-30 11:04:23 +0530

    feat: Add routes for property and cleaner retrieval

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,6 +3,10 @@ import {
   getCleaningRequestById,
   updateCleaningRequest,
   deleteCleaningRequest,
+  getAllProperty,
+  getPropertyById,
+  getAllCleaner,
+  getCleanerById,
 } from "../controllers/admin.controller.js";
 import { registerCleaner } from "../controllers/auth.controller.js";
 import { Router } from "express";
@@ -26,5 +30,10 @@ router
   .get(getCleaningRequestById)
   .put(updateCleaningRequest)
   .delete(deleteCleaningRequest);
+
+router.route("/property").get(getAllProperty);
+router.route("/property/:propertyId").get(getPropertyById);
+router.route("/cleaner").get(getAllCleaner);
+router.route("/cleaner/:cleanerId").get(getCleanerById);
 
 export default router;
