aede673d9816bc2e04c9f6b6b71391c821165389
achint <achintj96244@gmail.com>
2024-06-01 16:20:08 +0530

    chore: Update express middleware limits for JSON and URL-encoded data

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
