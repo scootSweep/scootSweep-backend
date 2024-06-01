0e864a80de260f70d3284758c3bfd99644020e10
achint <achintj96244@gmail.com>
2024-06-01 17:36:52 +0530

    feat: Add contact info endpoints for admin and implement cleaner registration by admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -7,6 +7,10 @@ import {
   getPropertyById,
   getAllCleaner,
   getCleanerById,
+  createContactInfo,
+  registerCleanerbyAdmin,
+  getAllContactInfo,
+  deleteContactInfo,
 } from "../controllers/admin.controller.js";
 import { registerCleaner } from "../controllers/auth.controller.js";
 import { Router } from "express";
@@ -16,7 +20,15 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 router.use(verifyAdmin);
 
-router.route("/register/cleaner").post(upload.none(), registerCleaner);
+// cleaner-routes
+router.route("/register/cleaner").post(
+  upload.single("idImage"), // Use upload.single middleware for single file uploads with the field name "idImage"
+  (req, res, next) => {
+    next();
+  },
+  registerCleanerbyAdmin
+);
+// router.route("/register/cleaner").post(upload.none(), registerCleaner);
 
 router.route("/cleaning_requests").get(getAllCleaningRequests);
 router
@@ -29,5 +41,8 @@ router.route("/property").get(getAllProperty);
 router.route("/property/:propertyId").get(getPropertyById);
 router.route("/cleaner").get(getAllCleaner);
 router.route("/cleaner/:cleanerId").get(getCleanerById);
+
+router.route("/contact").post(createContactInfo).get(getAllContactInfo);
+router.route("/contact/:contactInfoId").delete(deleteContactInfo);
 
 export default router;
