d577fce248e9602f1e4e994b21329f3afb02d2e8
achint <achintj96244@gmail.com>
2024-06-02 22:40:58 +0530

    refactor: Remove commented out code in cleaner.routes.js

--- src/routes/cleaner.routes.js
+++ src/routes/cleaner.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyCleaner } from "../middlewares/auth.middleware.js";
 import cleanerService from "../services/cleaner.service.js";
 const router = Router();
-router.use(verifyCleaner);
+// router.use(verifyCleaner);
 
 router.route("/property-location").get(cleanerService.getAllPropertyLocation);
 
