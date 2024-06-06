838393e68e87dd05cc7a972bc4eb0aaffe0a6efb
achint <achintj96244@gmail.com>
2024-06-06 22:46:40 +0530

    feat: Add Counter model for managing counters

---
+++ src/models/counter.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const counterSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  seq: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const Counter = mongoose.model("Counter", counterSchema);
