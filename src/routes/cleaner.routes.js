4b1011eb4841ed1a9972c53d54de738695265a66
achint <achintj96244@gmail.com>
2024-06-07 13:56:52 +0530

    refactor: Update cleaner.routes.js to include cleaning invoice route and middleware

--- src/routes/cleaner.routes.js
+++ src/routes/cleaner.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyCleaner } from "../middlewares/auth.middleware.js";
 import cleanerService from "../services/cleaner.service.js";
+import { createCleaningInvoice } from "../controllers/cleaningInvoice.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
-// router.use(verifyCleaner);
 
 router.route("/property-location").get(cleanerService.getAllPropertyLocation);
+
+router.use(verifyCleaner);
+router.route("/cleaning-invoice").post(upload.none(), createCleaningInvoice);
 
 export default router;
