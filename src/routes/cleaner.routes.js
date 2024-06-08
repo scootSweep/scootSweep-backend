bcd19b6b294cc5ccba9a759334241ca0b568642e
achint <achintj96244@gmail.com>
2024-06-09 03:55:46 +0530

    refactor: Update cleaner.routes.js to remove commented out code and add new routes for cleaning invoice, door OTP, and redeployment

--- src/routes/cleaner.routes.js
+++ src/routes/cleaner.routes.js
@@ -1,13 +1,24 @@
 import { Router } from "express";
 import { verifyCleaner } from "../middlewares/auth.middleware.js";
 import cleanerService from "../services/cleaner.service.js";
-import { createCleaningInvoice } from "../controllers/cleaningInvoice.controller.js";
+// import { createCleaningInvoice } from "../controllers/cleaningInvoice.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/property-location").get(cleanerService.getAllPropertyLocation);
 
 router.use(verifyCleaner);
-router.route("/cleaning-invoice").post(upload.none(), createCleaningInvoice);
+// router.route("/cleaning-invoice").post(upload.none(), createCleaningInvoice);
+router
+  .route("/cleaning-invoice")
+  .post(upload.none(), cleanerService.createCleaningInvoice);
+
+// send otp to open the door
+router.route("/door-otp").get(cleanerService.doorOtp);
+router.route("/verify-door").post(cleanerService.verifyDoorOtp);
+
+router
+  .route("/redeployment")
+  .post(upload.single("deviceIDImage"), cleanerService.createRedeployment);
 
 export default router;
