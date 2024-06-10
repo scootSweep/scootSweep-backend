87c189516175db7b56955596cf5760dcfeb4ba7e
achint <achintj96244@gmail.com>
2024-06-10 18:34:56 +0530

    refactor: Increase limit for JSON and URL encoded data to 20mb in app.js

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
