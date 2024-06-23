25896d52496e428268568bb81acd66224666654d
achint <achintj96244@gmail.com>
2024-06-23 19:19:59 +0530

    feat: Update package dependencies to include libphonenumber-js v1.11.4

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,19 +1,29 @@
+// const isValidPhoneNumber = (phone) => {
+//   // Remove any non-digit characters from the phone number
+//   const cleanedPhone = phone.replace(/\D/g, "");
+
+//   // Check if the cleaned phone number has 10 digits (without country code)
+//   if (/^\d{10}$/.test(cleanedPhone)) {
+//     return true;
+//   }
+
+//   // Check if the cleaned phone number has 12 digits (with country code)
+//   if (/^\+?\d{12}$/.test(cleanedPhone)) {
+//     return true;
+//   }
+
+//   // Otherwise, return false
+//   return false;
+// };
+
+// const { parsePhoneNumberFromString } = require('libphonenumber-js');
+import parsePhoneNumberFromString from "libphonenumber-js";
+
 const isValidPhoneNumber = (phone) => {
-  // Remove any non-digit characters from the phone number
-  const cleanedPhone = phone.replace(/\D/g, "");
+  const phoneNumber = parsePhoneNumberFromString(phone);
 
-  // Check if the cleaned phone number has 10 digits (without country code)
-  if (/^\d{10}$/.test(cleanedPhone)) {
-    return true;
-  }
-
-  // Check if the cleaned phone number has 12 digits (with country code)
-  if (/^\+?\d{12}$/.test(cleanedPhone)) {
-    return true;
-  }
-
-  // Otherwise, return false
-  return false;
+  // Check if the parsed phone number is valid
+  return phoneNumber && phoneNumber.isValid();
 };
 
 export { isValidPhoneNumber };
