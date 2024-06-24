20bc75156ef30e8645ef6d4f7daa1eb337c69d1f
achint <achintj96244@gmail.com>
2024-06-25 00:50:22 +0530

    feat: Add IllegalDockless model for managing illegal dockless vehicles

---
+++ src/models/illegalDockless.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const illegalDocklessSchema = new mongoose.Schema(
+  {
+    cleaner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cleaner",
+      required: true,
+    },
+    // reason: {
+    //   type: String,
+    //   enum: [
+    //     "Unauthorized Parking",
+    //     "Private Property",
+    //     "unauthorized parking",
+    //     "private property",
+    //   ],
+    //   required: true,
+    // },
+    issueDate: {
+      type: Date,
+      default: Date.now,
+    },
+    operatorName: {
+      type: String,
+      enum: ["Bird", "Lime", "Veo", "Spin"],
+      required: true,
+    },
+    deviceID: {
+      type: String,
+      required: true,
+    },
+    deviceIDImage: String,
+    // unauthorizedParkingImage: String,
+    gpsLocation: {
+      type: String,
+      required: true,
+    },
+    // authorizationStatus: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+    invoiceStatus: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["Paid", "Unpaid"],
+      default: "Unpaid",
+    },
+    paymentDate: Date,
+    // paymentAmount: Number,
+    additionalNotes: String,
+    invoiceNumber: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+export const IllegalDockless = mongoose.model(
+  "IllegalDockless",
+  illegalDocklessSchema
+);
