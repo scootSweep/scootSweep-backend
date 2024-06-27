9808f9bf54c32737043693e1bfa84a39c06a10e2
achint <achintj96244@gmail.com>
2024-06-28 00:05:03 +0530

    refactor: Update package dependencies to include body-parser v1.20.2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
@@ -10,7 +12,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+app.use(bodyParser.json());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.static("public"));
@@ -28,6 +30,25 @@ app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/admin", admin);
 app.use("/api/v1/cleaner", cleaner);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({
+      status_code: err.statusCode,
+      message: err.message,
+      title: "Error",
+      exception: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status_code: 500,
+      message: "Internal Server Error",
+      title: "Error",
+      exception: err.message,
+    });
+  }
+});
 
 // http://localhost:8000/api/v1/users/register
 
