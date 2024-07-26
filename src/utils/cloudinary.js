0f7f697ef708f2c280200391f506fd96a01b1324
achint <achintj96244@gmail.com>
2024-07-27 00:13:56 +0530

    feat: Add image and PDF upload functionality to cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import { ApiError } from "../utils/ApiError.js";
 import fs from "fs";
 
 cloudinary.config({
@@ -13,6 +14,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
+      public_id: `images/properties_${Date.now()}`,
     });
     // file has been uploaded successfull
     //console.log("file is uploaded on cloudinary ", response.url);
@@ -20,8 +22,33 @@ const uploadOnCloudinary = async (localFilePath) => {
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
-    return null;
+    throw new ApiError(500, "Error uploading image to cloudinary");
   }
 };
 
-export { uploadOnCloudinary };
+const uploadPDFBuffer = async (pdfBuffer) => {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          resource_type: "raw",
+          public_id: `pdfs/properties_${Date.now()}`,
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        }
+      );
+      stream.end(pdfBuffer);
+    });
+
+    return response.secure_url;
+  } catch (error) {
+    throw new ApiError(500, "Error uploading pdf to cloudinary");
+  }
+};
+
+export { uploadOnCloudinary, uploadPDFBuffer };
