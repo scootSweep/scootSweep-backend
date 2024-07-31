d8c0a15edfbd3601372430b31de87798a7101e10
achint <achintj96244@gmail.com>
2024-07-31 18:58:53 +0530

    feat: Update puppeteer launch options for improved security

--- src/services/property.service.js
+++ src/services/property.service.js
@@ -142,7 +142,9 @@ const createProperty = async (propertyData) => {
     time,
   });
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent);
   const pdfBuffer = await page.pdf({ format: "A4" });
